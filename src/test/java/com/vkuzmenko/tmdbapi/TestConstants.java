2a99103b5091571cda6df7c024f67367baaef3c8
Vit <test@epam.com>
2020-12-07 19:26:00 +0200

    Some refactoring

--- src/test/java/com/vkuzmenko/tmdbapi/TestConstants.java
+++ src/test/java/com/vkuzmenko/tmdbapi/TestConstants.java
@@ -1,12 +1,11 @@
 package com.vkuzmenko.tmdbapi;
 
 public class TestConstants {
-
   private TestConstants() {
   }
 
-  public static final String API_KEY = "0e3a0b2d5d7e580f9cc771bea41da413";
+  public static final String API_KEY_ENV = System.getenv("TMDB_API_KEY");
+  public static final String API_KEY = API_KEY_ENV != null ? API_KEY_ENV : "132123";
   public static final String API_VERSION = "3";
-
   public static final String API_ERROR_NOT_FOUND = "Response status error: 34 - The resource you requested could not be found.";
 }
